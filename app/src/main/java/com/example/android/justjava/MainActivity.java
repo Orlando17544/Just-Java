07e4017923885d366935f34785c69bf9d5eedecc
Orlando Canchola Jimenez <orcj17544@gmail.com>
2022-07-24 00:30:30 -0500

    App finished

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        String priceMessage = "Total: $" + (quantity * 5) + "\nThank you!";
+        displayMessage(priceMessage);
+        //displayPrice(quantity * 5);
     }
 
     /**
@@ -51,5 +53,13 @@ public class MainActivity extends AppCompatActivity {
             quantity = quantity - 1;
             display(quantity);
         }
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
